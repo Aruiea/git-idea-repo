92527bb619e44c53fac9cbc231f84292abf97d3c
Aruiea <943797537@qq.com>
2020-07-11 15:23:03 +0800

    first version1.0

---
+++ AruieaProject/src/com/aruiea/month07/day10/annotation/GitTest.java
@@ -0,0 +1,19 @@
+package com.aruiea.month07.day10.annotation;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * instruction of project
+ *
+ * @author Aruiea.com
+ * @ClassName GitTest
+ * @Version 1.0
+ * @Date 2020/7/11 15:21
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("GIT");
+    }
+
+}
